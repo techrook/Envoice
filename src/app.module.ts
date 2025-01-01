9e0a9f2d8e2894e57e59ca6cf0b2910519f935ae
techrook <mondayitohowo3@gmail.com>
2025-01-01 14:24:47 +0100

    database initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configuration, validate } from '../config/configuration';
 import { LoggerModule } from './common/log/logger.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -11,7 +12,8 @@ import { LoggerModule } from './common/log/logger.module';
     load: [configuration],
       validate,
   }),
-  LoggerModule,],
+  LoggerModule,
+  PrismaModule,],
   controllers: [AppController],
   providers: [AppService],
 })
