fcd281f30157ef1ecc0fddce9b425315a9894b7a
techrook <mondayitohowo3@gmail.com>
2025-01-16 16:56:50 +0100

    merge conflict

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,32 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { EventsManagerModule } from 'src/common/events/events.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './JWT Strategy/jwt.strategy';
 import { PrismaClient } from '@prisma/client';
-import { TwitterStrategy } from './strategy/twitter.strategy';
+
 
 @Module({
   imports: [
-    forwardRef(() => UsersModule),
-    PrismaModule, // Retain the PrismaModule from login-endpoint
+    UsersModule,
+    PrismaModule,
+    EventsManagerModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      global: true, // Retain the JWT configuration from login-endpoint
+      secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaClient, TwitterStrategy,],
+  providers: [
+    AuthService,
+    PrismaClient,
+    JwtStrategy,
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
