3f4e3cc2528bd78f00851e601eb5a9388c938ed3
caesarkutaa <caesarlouis0@gmail.com>
2026-01-08 09:31:16 +0100

    added resend email services

--- src/common/email/email.module.ts
+++ src/common/email/email.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from 'src/auth/JWT Strategy/jwt.strategy';
@@ -9,25 +8,10 @@ import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Ensure ConfigModule is set up
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get<string>('MAIL_HOST'),
-          port: config.get<number>('MAIL_PORT'), // 587
-          secure: false, // ← Must be false for port 587
-          requireTLS: true, // ← Ensures STARTTLS is used
-          auth: {
-            user: config.get<string>('MAIL_USERNAME'),
-            pass: config.get<string>('MAIL_PASSWORD'),
-          },
-          debug: true,
-        },
-        
-      }),
-    }),
+    ConfigModule.forRoot(), 
+  
   ],
-  providers: [EmailService, PrismaService,JwtStrategy,UsersService],
+  providers: [EmailService, PrismaService, JwtStrategy, UsersService],
+  exports: [EmailService], 
 })
 export class EmailModule {}
